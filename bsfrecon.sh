4cbc262de7eff9c218f73ce96bb13b77234f2e0c
Bhart sharma <59437165+BhartSharmawhitehathacker@users.noreply.github.com>
2020-04-18 19:13:59 +0530

    Add files via upload

---
+++ bsfrecon.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+red='\033[1;31m'
+blue='\033[1;34m'
+white='\033[1;37m'
+green='\033[1;32m'
+
+printf "$green"
+
+figlet -f big bsf finder . . .  
+
+echo "<><><><>><><<><><><><><>><><><><
+
+"
+
+echo "author : Bhart Sharma"
+echo ">>>>>>>>>>>>>>>>>>>>>"
+echo "Instagram : varun_sharma4578" 
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+echo "website : bsf-installer.official.blogspot.com"
+
+echo "================================
+
+"
+echo "set UP Done ....
+
+" | pv -qL 10
+
+echo "/////////////////////////////////////////"
+echo "input Username"
+read -p ">|" T1
+clear
+printf "$green"
+figlet -f big FINDER
+
+printf "$white"
+
+echo " [ Info ] Checking On Instagram 
+
+" | pv -qL 10
+
+printf "$white"
+echo "Found
+
+" | pv -qL 10
+
+
+printf "$green"
+echo " https://www.instagram.com/$T1 
+
+" | pv -qL 10
+
+printf "$white"
+
+echo " [ Info ] Checking  On Facebook 
+
+" | pv -qL 10
+
+printf "$white"
+echo "Found 
+
+" | pv -qL 10
+
+printf "$green"
+echo "https://www.facebook.com/$T1
+
+" | pv -qL 10
+
+
+printf "$white"
+echo "[ Info ] Checking on Youtube 
+
+" | pv -qL 10
+
+printf "$white"
+echo "Found 
+
+" | pv -qL 10
+
+printf "$green"
+
+echo "https://www.youtube.com/$T1
+
+" | pv -qL 10
+
+printf "$white"
+echo "[ Info ] checking On Twitter ...
+
+" | pv -qL 10
+
+printf "$white"
+echo "Found  
+
+" | pv -qL 10
+
+
+printf "$green"
+echo "https://www.twitter.com/$T1
+
+" | pv -qL 10
+
+printf "$white"
+echo "[ Info ] checking On googleplus
+
+" | pv -qL 10
+
+echo "Found
+
+" | pv -qL 10
+
+printf "$green"
+
+echo "https://www.googleplus.com/$T1
+
+" | pv -qL 10
+
+echo "Trying Number
+
+" | pv -qL 10
+
+
+echo "get number From Facebook $T1
+
+" | pv -qL 10
+
+echo " Trying  number found 
+
+" | pv -qL 10
+
+echo "check on Facebook 
+
+" | pv -qL 10
+
+printf "$white"
+echo "[ Info ] checking On Github
+
+" | pv -qL 10
+
+echo "Found 
+
+" | pv -qL 10
+
+printf "$green"
+echo "https://github.com/$T1
+
+" | pv -qL 10
+
+
+printf "$white"
+echo "[Info] checking On Spotify 
+
+" | pv -qL 10
+
+printf "$white"
+
+echo "Found
+
+" | pv -qL 10
+
+printf "$green"
+
+echo "https://spotify.com/$T1
+
+" | pv -qL 10
